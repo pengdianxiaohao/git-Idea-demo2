a1fd3a7e89d0644b664a5a07066b2246642ceb30
penghao <penghao@plp.com>
2022-07-29 13:48:41 +0800

    first commit

---
+++ src/main/java/com/penghao/GitIdeaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.penghao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIdeaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIdeaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/penghao/GitIdeaDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.penghao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testGit(){
+        System.out.println("hello,git");
+    }
+
+}
